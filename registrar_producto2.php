cf1925c36fdd331cbb32e121b12d9bf41b6bda49
CeciliaAlviz <cecycla_22@hotmail.es>
2017-05-19 21:40:53 -0500

    Add files via upload

---
+++ registrar_producto2.php
@@ -0,0 +1,42 @@
+<?php
+
+error_reporting(E_ALL);
+
+include("conexion.php");
+
+$id=addslashes(utf8_decode($_POST['id']));
+$codigo=addslashes(utf8_decode($_POST['codigo']));
+$nombre=addslashes(utf8_decode($_POST['nombre']));
+$descripcion=addslashes(utf8_decode($_POST['descripcion']));
+$precio=addslashes(utf8_decode($_POST['precio']));
+$categoria=addslashes(utf8_decode($_POST['categoria']));
+$almacen=addslashes(utf8_decode($_POST['almacen']));
+
+try{
+	$con=conectarse();
+	$result=$con->query("INSERT INTO productos(ID_productos, codigoProducto, nombre, descripcion, precioVenta, ID_categoria, ID_almacen) 
+	VALUES ('$id', '$codigo','$nombre', '$descripcion', '$precio', '$categoria', '$almacen')");
+	
+	if($result)
+	{
+		?>
+
+			<div class="text-success text-center">
+				<p>Operaci&oacute; realizada con &eacute;xito</p>
+			</div>
+
+			<?php
+	}
+	else
+	{
+		?>
+			<div class="text-danger text-center">
+				<p>Operaci&oacute;n no realizada</p>
+			</div>
+
+			<?php
+	}
+}
+catch(Exception $e){
+	throw $e;
+}
